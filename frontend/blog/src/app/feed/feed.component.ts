92babe3627bf2e06f6424a597512b2b1c88c2c25
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-10-09 17:34:34 -0300

    Create model and service

--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../service/post.service';
+import { Post } from '../model/Post';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  listPost: Post[] | undefined;
+  post: Post = new Post;
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.findPosts()
   }
 
+  findPosts(){
+    this.postService.getPosts().subscribe((data: any)=>{
+      this.listPost = data;
+    })
+  }
+
+  createMessage(){
+    this.postService.postMessage(this.post).subscribe((data: any)=> {
+      this.post = data;
+      location.assign('/feed')
+    })
+  }
+
+  deleteMessage(){
+    this.postService.deleteMessage(this.deleteMessage).subscribe((data: any)=>{
+      this.deleteMessage = data;
+      location.assign('/feed')
+    })
+  }
 }
---
+++ frontend/blog/src/app/model/Post.ts
@@ -0,0 +1,5 @@
+export class Post {
+  public id: number | undefined;
+  public name: string | undefined;
+  public mensagem: string | undefined;
+}
